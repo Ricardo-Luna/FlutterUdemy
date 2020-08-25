8f24ec2190d947c6558f81ad42834aec8cb6b43a
Ricardo Luna Torres <ricardo.luna@zapotlan.gob>
2020-08-25 14:48:20 -0500

    map view done!

--- S9-QR-Maps/qrreaderapp/lib/src/pages/mapa_page.dart
+++ S9-QR-Maps/qrreaderapp/lib/src/pages/mapa_page.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_map/flutter_map.dart';
+//import 'package:latlong/latlong.dart';
+
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
 class MapaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Coordenadas QR'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.my_location), onPressed: () {})
-        ],
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(scan.valor),
-      ),
+        appBar: AppBar(
+          title: Text('Coordenadas QR'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.my_location),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: _crearFlutterMap(scan));
+  }
+
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(center: scan.getLatLng(), zoom: 10),
+      layers: [
+        _crearMapa(),
+      ],
     );
+  }
+
+  _crearMapa() {
+    return TileLayerOptions(
+        urlTemplate:
+            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+        additionalOptions: {
+          'accessToken':
+              'pk.eyJ1Ijoicmlja3lsdW5hdCIsImEiOiJja2VhYXU0Y3AyZXlhMnpwOHFjY2dpOXFlIn0.n39zAClv937B2oA7BfCb7g',
+          'id': 'mapbox/satellite-streets-v11'
+        });
   }
 }
